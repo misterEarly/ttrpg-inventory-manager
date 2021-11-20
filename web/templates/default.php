4992aa607e5e784015ed1ada304c92d6764a34b7
misterEarly <afi.lovisfrueh@gmail.com>
2021-11-20 17:21:28 +0100

    added some files and templating engine

--- web/templates/default.php
+++ web/templates/default.php
@@ -5,20 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="nunjucks.js"></script>
-
 </head>
 <body>
-
-    {% extends "web/templates/default" %}
-
-    {% block content %}
-        <div id="content" class="container internal">
-            <h1>Home</h1>
-        </div>
-    {% endblock %}
-
+    <header>
+        Hello
+    </header>
+    
+    
+    {% block content %}{% endblock %}
 </body>
 </html>
-
-
